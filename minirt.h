5bf06964cd27fac8d9633f536433b03478cd6b81
maemi <dbsrl759@naver.com>
2022-07-03 23:42:26 +0900

    [init] miniRT

---
+++ minirt.h
@@ -0,0 +1,14 @@
+#ifndef MINIRT_H
+# define MINIRT_H
+
+# include <unistd.h>
+// write read
+# include <stdlib.h>
+// malloc free exit
+# include <stdio.h>
+// printf perror strerror
+# include <fcntl.h>
+// open close read
+# include <math.h>
+
+#endif
